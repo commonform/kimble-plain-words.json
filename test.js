565245d6bdb94fb21fdf5306a6843d64a32608b7
Kyle E. Mitchell <kyle@kemitchell.com>
2021-05-18 19:11:33 -0700

    Drop lispy-json

--- test.js
+++ test.js
@@ -6,9 +6,8 @@ const result = tv4.validateMultiple(data, schema)
 if (result.valid) {
   process.exit(0)
 } else {
-  const lispy = require('lispy-json')
-  process.stderr.write(lispy(result.errors) + '\n')
-  process.stderr.write(lispy(result.missing) + '\n')
+  process.stderr.write(JSON.stringify(result.errors, null, 2) + '\n')
+  process.stderr.write(JSON.stringify(result.missing, null, 2) + '\n')
   process.stderr.on('drain', function () {
     process.exit(1)
   })
