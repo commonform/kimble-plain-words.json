aa9e406ea81e3d1b91f50e3d98ea307ea5211091
Kyle E. Mitchell <kyle@kemitchell.com>
2021-05-18 19:10:43 -0700

    npx standard --fix test.js

--- test.js
+++ test.js
@@ -1,13 +1,15 @@
-var tv4 = require('tv4')
-var data = require('./')
-var schema = require('./schema.json')
+const tv4 = require('tv4')
+const data = require('./')
+const schema = require('./schema.json')
 
-var result = tv4.validateMultiple(data, schema)
+const result = tv4.validateMultiple(data, schema)
 if (result.valid) {
-  process.exit(0) }
-else {
-  var lispy = require('lispy-json')
+  process.exit(0)
+} else {
+  const lispy = require('lispy-json')
   process.stderr.write(lispy(result.errors) + '\n')
   process.stderr.write(lispy(result.missing) + '\n')
-  process.stderr.on('drain', function() {
-    process.exit(1) }) }
+  process.stderr.on('drain', function () {
+    process.exit(1)
+  })
+}
